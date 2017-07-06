1ce070e609c14819267fb7a800179bb6a067a1ca
MariBori <maryna.khmara@gmail.com>
2017-07-06 17:00:55 +0300

    Final version

--- src/test/java/Task_3.java
+++ src/test/java/Task_3.java
@@ -1,46 +1,46 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 
 public class Task_3 {
 
-    WebDriver driver1;
-    WebDriver driver2;
+    private WebDriver driver;
+    String baseUrl = "http://google.com";
 
-    @Before
-    public void start(){
-        driver1 = new FirefoxDriver();
-    }
-
-    @Test
-    public void google_test()throws InterruptedException{
-        driver1.get("http://google.com");
-        Thread.sleep(3000);
-    }
-
-    @After
-    public void stop(){
-        driver1.quit();
+    @BeforeClass
+    public static void setup() {
+        FirefoxDriverManager.getInstance().setup();
+        ChromeDriverManager.getInstance().setup();
     }
 
     @Before
-    public void start_browser(){
-        driver2 = new ChromeDriver();
-    }
+    public void setupdriver() throws Exception {
+        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        }
 
-    @Test
-    public void googletest()throws InterruptedException{
-        driver2.get("http://google.com");
-        Thread.sleep(3000);
-    }
 
-    @After
-    public void stop_browser(){
-        driver2.quit();
+        @Test
+        public void google_test() throws Exception {
+                driver.get(baseUrl);
+            }
 
+
+        @After
+        public void stop() throws Exception {
+            if ( driver != null) {
+                driver.quit();
+            }
+
+        }
     }
-}
